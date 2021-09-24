02b4a902a3582568a3cded441c7b9f2334eaa588
scarlett-liang <86292092+scarlett-liang@users.noreply.github.com>
2021-09-24 13:37:30 +0200

    added dictionary api

--- src/search.js
+++ src/search.js
@@ -1,23 +1,37 @@
-import {useState} from 'react';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-export default function Search(){
-    const [keyword,setKeyword]=useState("");
-    function handleSubmit(event){
-        event.preventDefault();
-        alert(`Searching for ${keyword}`);
-    }
-    function handleChange(event){
-        setKeyword(event.target.value);
-    }
-    return (
-        <div className="Search">
-            <form onSubmit={handleSubmit}>
-                <div className="row g-3 text-center">
-                 <input type="search" className="col-8 ms-5" placeholder="What word do u wanna look up?" autoFocus={true} onChange={handleChange}/>
-                 <button type="submit" className="btn btn-primary col ms-1 me-5">Search</button>
-                </div>
-            </form>
+export default function Search() {
+  const [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <div className="row g-3 text-center">
+          <input
+            type="search"
+            className="col-8 ms-5"
+            placeholder="What word do u wanna look up?"
+            autoFocus={true}
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn btn-primary col ms-1 me-5">
+            Search
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
